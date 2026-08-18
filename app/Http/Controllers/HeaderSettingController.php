284996a78c412e718a3312f550d6491eb081114d
motazami-dev <mantazami.dev@gmail.com>
2026-08-19 07:10:50 +0800

    z

--- app/Http/Controllers/HeaderSettingController.php
+++ app/Http/Controllers/HeaderSettingController.php
@@ -12,7 +12,7 @@ class HeaderSettingController extends Controller
         return view('admin.logo.create');
     }
     public function store(Request $request){
-        dd($request->all());
+        // dd($request->all());
         $fullName=$request->logo_path->getClientOriginalName();
         $path=$request->file("logo_path")->storeAs("logo",$fullName,"public");
         headerSetting::ubsert(['logo_path'=>$path],["id"],['logo_path']);
