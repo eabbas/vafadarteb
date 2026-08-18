b238b832740ad3d3834fd7a290dc4fa27aaff8a5
motazami-dev <mantazami.dev@gmail.com>
2026-08-19 07:20:19 +0800

    debug

--- app/Http/Controllers/HeaderSettingController.php
+++ app/Http/Controllers/HeaderSettingController.php
@@ -16,5 +16,6 @@ class HeaderSettingController extends Controller
         $fullName=$request->logo_path->getClientOriginalName();
         $path=$request->file("logo_path")->storeAs("logo",$fullName,"public");
         headerSetting::upsert(['logo_path'=>$path],["id"],['logo_path']);
+        return redirect('/');
     }
 }
