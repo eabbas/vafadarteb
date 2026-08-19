81c79aa2c44d546e0193ba82cd72361d25b4dd57
motazami-dev <mantazami.dev@gmail.com>
2026-08-19 21:18:12 +0800

    controller model migration is added and the fillable and migration is complated

---
+++ app/Models/hero.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class hero extends Model
+{
+    protected $fillable=[
+        'title',
+        'summary',
+        'addres',
+        'background_image_path',
+        'hero_image_path',
+    ];
+}
