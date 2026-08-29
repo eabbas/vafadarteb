7aa18abc32e3a1c8c8c414743226455e11dbe2fa
motazami-dev <mantazami.dev@gmail.com>
2026-08-29 18:27:41 +0800

    debuged some things

--- app/Http/Middleware/checkUserExist.php
+++ app/Http/Middleware/checkUserExist.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use App\Models\phone_code;
 
 class checkUserExist
 {
@@ -22,8 +23,17 @@ class checkUserExist
         $user=User::where('phoneNumber',$request->phoneNumber)->first();
         // dd($user);
         if($user){
-            if(Hash::check($request->password,$user->password)){
-                return $next($request);
+            if(isset($request['code'])){
+                $phone_code=phone_code::where('phoneNumber',$request->phoneNumber)->first();
+                if($phone_code){
+                    if($phone_code->code == $request->code && $request->phoneNumber == $phone_code->phoneNumber){
+                        return $next($request);
+                    }
+                }
+            }else{
+                if(Hash::check($request->password,$user->password)){
+                    return $next($request);
+                }
             }
         }
         return to_route('user.loginPage');
